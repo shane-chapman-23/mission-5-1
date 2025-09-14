ed1800192c52ac52aac87d6d19b6991d2dfbf7e4
shane-chapman-23 <chapmanshane23@gmail.com>
2025-09-15 11:11:42 +1200

    Added mock data to auctions.js

---
+++ mission-5-1/data/auctions.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    title: "TV",
+    description: "42 inch OLED TV",
+    start_price: 300,
+    reserve_price: 400,
+  },
+  {
+    title: "The Three-Body Problem",
+    description: "Book by Liu Cixin, 2006",
+    start_price: 20,
+    reserve_price: 20,
+  },
+  {
+    title: "Nintendo Switch",
+    description: "OLED Nintendo Switch",
+    start_price: 200,
+    reserve_price: 250,
+  },
+  {
+    title: "Rare Tech Deck Collection",
+    description: "Over 50 rare tech decks",
+    start_price: 1000,
+    reserve_price: 1050,
+  },
+];
--- mission-5-1/index.js
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env node
-const {program} = require("commander");
-
-program.command("seed").description("Seed data into the database").action(seed);
